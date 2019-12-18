2ee86135ec7689737425a325724899ec6a7fbd20
krzysiek2222 <56931577+krzysiek2222@users.noreply.github.com>
2019-12-18 22:18:37 +0100

    Zapytania REST
    - GET, POST, PUT, DELETE.
    - Sprawdzenie tokena i na jego podstawie wyświetlanie listy z GET dla danego użytkownika
    - Przypisywanie poprzez decode  z tokenu user-id do wstawianego taska.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,27 +1,56 @@
-const {Task} = require('../models/tasks')
-const mongoose = require('mongoose');
+const {Task, validate} = require('../models/tasks')
+const validateObjectId = require('../middleware/validateObjectId');
 const express=require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-router.get('/', async (req, res) => {
-    const tasks = await Task.find().sort('task_name');
+
+router.get('/', auth, async (req, res) => {
+    const tasks = await Task.find({user_id: req.user._id}).sort('task_name');
     res.send(tasks);
   });
 
-router.post('/', async(req, res)=>{
-    let task = new Task({
-        task_id: task.length+1,
-        task_name: req.body.task_name,
-        task_description: req.body.task_description,
-        task_end_date: req.body.task_end_date,
-        task_done: req.task_done,
-        user_id: 1 // TO DO
-    });
 
-    task = await task.save();    
-    
-    res.send(task);
+router.post('/', auth,  async (req, res)=>{
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+    let task = new Task({
+      task_name: req.body.task_name,
+      task_description: req.body.task_description,
+      task_end_date: req.body.task_end_date,
+      task_done: false,
+      user_id: req.user._id
+  });
+  task = await task.save();    
+  res.send(task);
 });
 
 
-  module.exports = router;
+router.put('/:id', [auth, validateObjectId], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  const task = await Task.findByIdAndUpdate(req.params.id,
+    { 
+      task_name: req.body.task_name,
+      task_description: req.body.task_description,
+      task_end_date: req.body.task_end_date,
+      task_done: req.body.task_done,
+    }, { new: true });
+
+  if (!task) return res.status(404).send('The task with given ID was not found.');
+  
+  res.send(task);
+});
+
+
+router.delete('/:id', [auth, validateObjectId], async (req, res) => {
+  const task = await Task.findByIdAndRemove(req.params.id);
+
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+
+  res.send(task);
+});
+
+module.exports = router;
