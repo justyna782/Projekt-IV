d151d96b0f04c5beae02f402c48bc72ea67720eb
krzysiek2222 <56931577+krzysiek2222@users.noreply.github.com>
2019-12-18 22:19:48 +0100

    Update 18.12.2019

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
+
 
 const Task = new mongoose.model('Task', new mongoose.Schema({
-    task_id: {
-        type: Number,
-        required: true
-    },
     task_name: {
         type: String,
         required: true
@@ -15,16 +13,29 @@ const Task = new mongoose.model('Task', new mongoose.Schema({
     },
     task_end_date: {
         type: Date,
+        required: true,
+        default: (new Date()).setDate((new Date()).getDate() + 2)
+    },
+    user_id: {
+        type: String,
         required: true
     },
     task_done: {
         type: Boolean,
-        required: true
-    },
-    user_id: {
-        type: Number,
-        required: true
+        default: false
     }
 }));
 
+function validateTask(task) {
+    const schema = {
+        task_done: Joi.boolean(),
+        task_name: Joi.string().min(1).required(),
+        task_end_date: Joi.date(),
+        task_description: Joi.string()
+    };
+    return Joi.validate(task, schema);
+}
+
+
 exports.Task = Task;
+exports.validate = validateTask;
